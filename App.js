93353bfc8ce26ce94218eaed27def27dbb13e3cd
amang <amangupta6121998@gmail.com>
2021-07-28 17:12:15 +0530

    updated : script for bday calcuation

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 const InputBirthday = document.getElementById("userBirthday");
 const InputLuckyNumber = document.getElementById("userLuckyNumber");
+const Check = document.getElementById("btn");
+const dispalyResult = document.getElementById("result-wrapper");
+const resultBackground = document.getElementById("resultcontainer");
+const resultInText = document.getElementById("resultshow");
+const closeResult = document.getElementById("close");
+let userDOBsum;
+let userLuckyNumber;
+const checkLucky = (DOBsum, LuckyNum) => {
+  if (DOBsum % LuckyNum === 0) {
+    console.log("lucky");
+    resultInText.innerHTML = "Your Birthday is A Lucky Number !!!";
+    resultBackground.style.backgroundImage =
+      "url('https://media.giphy.com/media/PMV7yRpwGO5y9p3DBx/giphy.gif')";
+  } else {
+    resultInText.innerHTML = "Your Birthday is Not A Lucky Number !!!";
 
-const checkLucky = (userBirthdaySum) => {
-  InputLuckyNumber.addEventListener("change", (e) => {
-    const userLuckyNum = parseInt(e.target.value);
-
-    if (userLuckyNum === userBirthdaySum && userLuckyNum !== "") {
-      console.log("lucky");
-    } else {
-      console.log("not lucky");
-    }
-  });
+    resultBackground.style.backgroundImage =
+      "url('https://media.giphy.com/media/lSzQjkthGS1gc/giphy.gif')";
+  }
 };
 
 const toDigit = (userBirthday) => {
@@ -22,8 +30,27 @@ const toDigit = (userBirthday) => {
     birthdaySum += holdBirthday;
     num = num / 10;
   }
-  checkLucky(birthdaySum);
+
+  userDOBsum = birthdaySum;
 };
+
+closeResult.addEventListener("click", () => {
+  dispalyResult.style.display = "none";
+});
+
+Check.addEventListener("click", () => {
+  if (userDOBsum >= 0 && userLuckyNumber >= 0) {
+    dispalyResult.style.display = "grid";
+    checkLucky(userDOBsum, userLuckyNumber);
+  } else {
+    alert("Enter valid number");
+  }
+});
+
+InputLuckyNumber.addEventListener("change", (e) => {
+  const userLuckyNum = parseInt(e.target.value);
+  userLuckyNumber = userLuckyNum;
+});
 
 InputBirthday.addEventListener("change", (e) => {
   const userInput = e.target.value;
